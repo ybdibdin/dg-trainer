37fbf6293abe4968a87e340695b3c6c55f5b2171
centmaster <861676131@qq.com>
2017-06-08 11:59:25 +0800

    6.8

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
+  <div class="nav">
     <el-menu theme="light" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
       <el-menu-item index="1">Processing Center</el-menu-item>
       <el-menu-item index="2"><a href="http://www.deepglint.com" target="_blank">Contact us</a></el-menu-item>
     </el-menu>
 
     <div id="form">
-      <el-form label-width="160px" class="demo-ruleForm">
+      <el-form label-width="180px" class="demo-ruleForm">
 
         <div class="upload">
-          <el-form :inline="true"  label-width="100px">
+          <el-form   label-width="180px">
             <el-form-item label="Model" required>
               <el-upload
                 action="https://jsonplaceholder.typicode.com/posts/"
@@ -28,7 +28,7 @@
               </el-upload>
             </el-form-item>
           </el-form>
-          <el-form :inline="true" label-width="100px">
+          <el-form  label-width="180px">
             <el-form-item label="TrainScript" required>
               <el-upload
                 action="https://jsonplaceholder.typicode.com/posts/"
@@ -83,24 +83,27 @@
 
 
 
-        <el-radio-group   class="checkboxshow">
-          <el-checkbox v-model="Gpus1">Gpus--1</el-checkbox>
-          <el-checkbox v-model="Cpus1">Cpus--1</el-checkbox>
-          <el-checkbox v-model="Mem16">Mem--16</el-checkbox>
-          <el-checkbox v-model="Disk256">Disk--256</el-checkbox>
-          <el-checkbox v-model="Instance1">Instance--1</el-checkbox>
-        </el-radio-group>
+
 
 
         <el-form-item class="submitbtn">
           <el-button type="primary" @click="submit">Create</el-button>
-          <el-button @click="resetForm('ruleForm')">Reset</el-button>
+          <el-button @click="deletetask">Delete</el-button>
         </el-form-item>
       </el-form>
     </div>
 
 
     <div id="logpanel">
+
+      <el-radio-group   class="checkboxshow">
+        <el-checkbox v-model="Gpus1">Gpus--1</el-checkbox>
+        <el-checkbox v-model="Cpus1">Cpus--1</el-checkbox>
+        <el-checkbox v-model="Mem16">Mem--16</el-checkbox>
+        <el-checkbox v-model="Disk256">Disk--256</el-checkbox>
+        <el-checkbox v-model="Instance1">Instance--1</el-checkbox>
+      </el-radio-group>
+
       <el-tabs v-model="activeName">
         <el-tab-pane label="task" name="first"><el-input
           type="textarea"
@@ -117,7 +120,7 @@
           :rows="25"
           v-model="text3">
         </el-input></el-tab-pane>
-        <el-tab-pane label="task info" name="fourth"><el-input
+        <el-tab-pane label="task info" name="forth"><el-input
           type="textarea"
           :rows="25"
           v-model="text4">
@@ -160,6 +163,7 @@
         Disk256:true,
         activeIndex: '1',
         activeName: 'first',
+        taskid:'',
         text1:'waiting ...',
         text2:'waiting ...',
         text3:'waiting ...',
@@ -174,10 +178,16 @@
     },
     watch:{
       activeName:function (val, oldval) {
-        if(val == 'second'){
+        if(val == 'second'||this.text2 == 'waiting ...'){
             this.second();
-        }else if(val == 'third'){
-
+        }else if(val == 'third'||this.text3 == 'waiting ...'){
+            this.third(taskid);
+        }else if(val == 'forth'||this.text4 == 'waiting ...'){
+          this.forth();
+        }else if(val == 'fifth'||this.text5 == 'waiting ...'){
+          this.fifth();
+        }else if(val == 'sixth'||this.text6 == 'waiting ...'){
+          this.sixth();
         }
       }
 
@@ -214,24 +224,78 @@
         form.append('TrainDataUri',this.TrainDataUri);
         form.append('TestDataUri',this.TestDataUri);
         form.append('PretrainedModelUri',this.PretrainedModelUri);
-        form.append('Gpus',1);
-        form.append('Cpus',1);
-        form.append('Mem',16);
-        form.append('Disk',256);
-        form.append('Instances',1);
-        var xhr = new XMLHttpRequest()
-        xhr.open('POST','http://192.168.6.66:8081/dgtrainer/v1/task', true)
-        xhr.onreadystatechange = function () {
-          if (xhr.readyState == 4 && xhr.status == 200) {
-            alert('success');
-          }
-        }
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-        xhr.send(form)
+        form.append('Gpus','1');
+        form.append('Cpus','1');
+        form.append('Mem','16');
+        form.append('Disk','256');
+        form.append('Instances','1');
+        this.$http({
+          url:'http://192.168.6.66:8081/dgtrainer/v1/task',
+          method:'post',
+          data:form,
+          headers: {'Content-Type': 'multiple/form-data'}
+        }).then((res)=>{console.log(res)})
+
       },
       second:function () {
+        this.$http.get('http://192.168.6.66:8081/dgtrainer/v1/tasks').then(response => {
+          // success callback
+          console.log(response)
+        }, response => {
+          // error callback
+        })
+      },
+      third:function (i) {
         var xhr = new XMLHttpRequest();
-        xhr.open('GET','http://192.168.6.66:8081/dgtrainer/v1/tasks', true);
+        xhr.open('GET','http://192.168.6.66:8081/dgtrainer/v1/task/'+this.taskid+'/logs', true);
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState == 4 && xhr.status == 200) {
+            console.log('success');
+          }
+        }
+        xhr.setRequestHeader("Content-Type", "application/json ;charset=utf-8");
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.send();
+      },
+      forth:function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET','http://192.168.6.66:8081/dgtrainer/v1/task/'+this.taskid, true);
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState == 4 && xhr.status == 200) {
+            console.log('success');
+          }
+        }
+        xhr.setRequestHeader("Content-Type", "application/json ;charset=utf-8");
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.send();
+      },
+      fifth:function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET','http://192.168.6.66:8081/dgtrainer/v1/images', true);
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState == 4 && xhr.status == 200) {
+            console.log('success');
+          }
+        }
+        xhr.setRequestHeader("Content-Type", "application/json ;charset=utf-8");
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.send();
+      },
+      sixth:function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET','http://192.168.6.66:8081/dgtrainer/v1/resources', true);
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState == 4 && xhr.status == 200) {
+            console.log('success');
+          }
+        }
+        xhr.setRequestHeader("Content-Type", "application/json ;charset=utf-8");
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.send();
+      },
+      deletetask:function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open('DELETE','http://192.168.6.66:8081/dgtrainer/v1/task/'+this.taskid+'?Status=333&Message=aaaaaaa', true);
         xhr.onreadystatechange = function() {
           if (xhr.readyState == 4 && xhr.status == 200) {
             console.log('success');
@@ -259,8 +323,8 @@
   margin-top: 30px;
 }
   .upload{
-    margin-top: 20px;
-    margin-left: 48px;
+    margin-top: 10px;
+    margin-left: 0;
   }
   .submitbtn{
     margin-left: 40px;
@@ -268,7 +332,11 @@
   }
   #logpanel{
     margin-left:50%;
-    margin-top: 20px;
+    margin-top: 0;
     width: 48%;
+  }
+  .nav{
+
+
   }
 </style>
